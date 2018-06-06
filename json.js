5047a69d7522fed829c55c0dbe9e52ab34bf608b
Zach Taylor <me@zacharytaylor.ca>
2018-06-05 21:49:24 -0400

    Added logic to chunk data parsed into question objects.
    Also improved the removeExtraLines method by renaming it and adding functionality to remove lines with only spaces and trimming lines.

--- json.js
+++ json.js
@@ -1,8 +1,40 @@
 function Convert() {
   //Get the text from the plainText input. 
-  var plaintextInput = removeExtraLines($('#plaintextInput').val().split('\n'));
+  var plaintextInput = formatPlainText($('#plaintextInput').val().split('\n'));
   var Questions = [];
 
+   
+   for (var i = 0; i < plaintextInput.length; i++) {
+      if (plaintextInput[i] != '') {
+         var Question = {
+            questionType: "",
+            maxScoreValue: 1,
+            questionText: "",
+            hintText: "none",
+            hintMedia: "none",
+            answers: []
+         }
+         var Answer = {
+            answerText: "",
+            feedBack: "",
+            scoreValue: 0
+         }
+         for (var j = 0; (plaintextInput[i + j] != '') && (i + j != plaintextInput.length); j++) {
+            if (j === 0) {
+               Question.questionText = plaintextInput[i];
+            } else {
+               Answer.questionText = plaintextInput[i + j];
+            Question.answers.push(Answer); 
+            }
+            
+         }
+         Questions.push(Question);
+         i = i + j;
+      }
+      
+   }
+   
+    console.log(Questions);
 
 
 
@@ -32,12 +64,19 @@ function Convert() {
 }
 
 //Searches through array of strings and removes empty lines if there's more than one of them in a row. 
-function removeExtraLines(array) {
+function formatPlainText(array) {
   var previousEnter = false;
   var redundantLines = [];
-
+  
   for (var i = 0; i < array.length; i++) {
-
+      //Sets any lines that are JUST spaces to ''
+    if (isSpaces(array[i])) {
+        array[i] = '';
+    }
+   
+     //Trim lines
+    array[i] = array[i].trim();
+    
     if (array[i] === '' && !previousEnter) {
       previousEnter = true;
     } else if (array[i] === '' && previousEnter) {
@@ -51,10 +90,20 @@ function removeExtraLines(array) {
     array.splice(redundantLines[j - 1], 1);
 
   }
-  //After clean up, removes the leading newline, if there is one. 
+  //After clean up, removes the leading or trailing newline, if there is one. 
+    if (array[array.length - 1] === '') {
+        array.splice(array.length - 1, 1);
+    }
   if (array[0] === '') {
     array.splice(0, 1);
   }
-
   return array;
+}
+
+function isSpaces(testString) {
+    var justSpaces = /^\s+$/;
+    if (justSpaces.test(testString)) {
+        return true;
+    }
+    return false;
 }
